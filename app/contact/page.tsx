a156a83a295e53be8e59239073a90dcf566bc7b5
riabrez <maria060804@gmail.com>
2025-11-02 20:52:20 +0000

    remove unnecessary folders

--- app/contact/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <div>Contact page (coming soon)</div>;
-}
--- app/projects/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <div>Projects page (coming soon)</div>;
-}
